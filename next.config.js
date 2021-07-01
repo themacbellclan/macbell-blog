ebe74459e5e396c3b0e8b4f939c802441134e86c
themacbellclan <86795521+themacbellclan@users.noreply.github.com>
2021-07-01 10:06:35 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    mongodb_username: "macbelluser",
-    mongodb_password: "macbell",
-    mongodb_clustername: "macbell-cluster",
-    mongodb_database: "macbell-blog",
-  },
+  
 };
